e747e12e6b2782416ee66d77239bf1f2327bc9e1
Ayman <enkhalifapro@gmail.com>
2021-05-19 12:58:48 +0200

    code refactor

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/egylinux/kf_go
+
+go 1.15
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/jmoiron/sqlx v1.3.4
+	github.com/lib/pq v1.10.2
+	github.com/urfave/cli/v2 v2.3.0
+)
--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"fmt"
+	"github.com/egylinux/kf_go/db"
+	"github.com/egylinux/kf_go/users"
 	_ "github.com/lib/pq"
 	"github.com/urfave/cli/v2"
 	"os"
 )
 
 func main() {
-	Connect()
 	//pass username,password from cli
 	var uname, pass string
 	app := cli.NewApp()
@@ -16,9 +17,30 @@ func main() {
 		&cli.StringFlag{Destination: &uname, Name: "u", Value: "root", Usage: "Specify username. Default is root"},
 		&cli.StringFlag{Destination: &pass, Name: "p", Value: "password", Usage: "Specify password. Default is password"},
 	}
-	error := app.Run(os.Args)
-	CheckError(error)
 
-	result := CheckUser(uname, pass)
-	fmt.Println(result)
+	if err := app.Run(os.Args); err != nil {
+		fmt.Println("Error: ", err.Error())
+		return
+	}
+
+	// build services
+	dbConnector,err := db.NewConnector("", "")
+	if err!=nil{
+		fmt.Println("DB Error: ", err.Error())
+		return
+	}
+
+	usersMgr := users.NewManager(dbConnector)
+
+	exist,err:=usersMgr.IsExist("","")
+	if err!=nil{
+		fmt.Println("User not found", err.Error())
+		return
+	}
+
+	if exist{
+		fmt.Println("User is found")
+	}
+	/*result := CheckUser(uname, pass)
+	fmt.Println(result)*/
 }
